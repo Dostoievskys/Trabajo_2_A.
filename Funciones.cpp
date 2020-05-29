47c947e8d0690543e50f639ca447c880c6515864
Jennifer Portiño <jennifer.portinof@utem.cl>
2020-05-29 19:54:19 -0400

    Merge: Jen - Master

--- Funciones.cpp
+++ Funciones.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 void participantes(){
-    std::cout << std::endl << "=== Ejemplo de Promediar ===" << std::endl;
+    std::cout << std::endl << "=== Integrantes: ===" << std::endl;
     std::cout << std::endl << "Braulio Argandoña"  << std::endl;
     std::cout << std::endl << "Fanny Rivero"  << std::endl;
     std::cout << std::endl << "Jennifer Portiño"  << std::endl;
@@ -216,4 +216,35 @@ bool ValidarRut(std::string rut){
   else{
     return false;
   }
+}
+/**
+ * Funcion de Ordenamiento
+*/
+void quicksort(Carrera x,float prim,float ult){ // quicksort(21041,0,vacantes-1);
+    Estudiante aux; //Requiere clase Estudiante dentro de Clase Carrera
+    int j,i,pivot;  //En teoria que postulantes[Vacantes] sea de tipo estudiante
+                    //Estudiante: Atributos Pond y rut
+                    //Funcion necesaria "GetPond" y "SetPond"
+                    //El agregar al final en el fichero general seguirìa en pie, slds <3
+    if(first<last){
+         pivot=prim;
+         i=prim;
+         j=ult;
+         while(i<j){
+             while(x.GetPostulantes[i].GetPond <= x.GetPostulantes[pivot].GetPond &&i<ult){
+                 i++;}
+             while(x.GetPostulantes[j].GetPond>x.GetPostulantes[pivot].GetPond){
+                 j--;}
+             if(i<j){
+                 aux=x.GetPostulantes[i];
+                  x.SetPostulantes[i]=x.GetPostulantes[j];
+                  x.SetPostulantes[j]=aux;
+             }
+         } 
+         aux=x.GetPostulantes[pivot];
+         x.SetPostulantes[pivot]=x.GetPostulantes[j];
+         x.SetPostulantes[j]=aux;
+         quicksort(x,prim,j-1);
+         quicksort(x,j+1,ult);
+    }
 }
--- Funciones.h
+++ Funciones.h
@@ -9,5 +9,9 @@ std::vector<Carrera> LlenarDatos();
 std::vector<int> obtenerPuntajes(std::string fila);
 float Ponderacion(Carrera C, std::vector<int> puntajes);
 bool ValidarRut(std::string rut);
+/**
+ * Funcion de Ordenamiento
+*/
+void quicksort(Carrera ,float,float);
 
 #endif
