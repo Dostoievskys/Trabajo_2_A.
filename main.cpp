2b5f9601621f87d59d3279dac8a2572d9185498d
Braulio <braulio.argandonac@utem.cl>
2020-05-28 20:03:38 -0400

    Validacion menu

--- main.cpp
+++ main.cpp
@@ -10,18 +10,31 @@ void participantes();
 
 int main(int argc, char** argv){
     if(argc > 1){
-        if(argv[1] == 1){ //Creacion de archivos
+        std::string op = argv[1];
+        if(op == "1" && argc == 4){ //argc = 4 o 3 (llamada;opcion;ubicacion;ubicacion)
             std::string archivo(argv[2]);
             std::ifstream lectura(archivo);
             std::ofstream escritura("puntajes.csv");
+            //ACA EJECUTAR OPCION 1
+            std::cout << std::endl << "CORRE 1" << std::endl;
+        }else{
+            if(op == "2" && argc == 4){
+                //ACA EJECUTAR OPCION 2: BUSQUEDA POR RUT
+                std::cout << std::endl << "CORRE 2" << std::endl;
+            }else{
+                if(argc != 4){
+                    if(op == "2" || op == "1"){
+                        std::cout << std::endl << "Ingrese la cantidad correcta de datos" << std::endl;
+                    }
+                }
+                if(op != "2" && op != "1"){
+                            std::cout << std::endl << "OPCION INVALIDA" << std::endl;
+                }      
+            }
         }
-        if(argv[1] == 2){
-            //Busqueda del rut
-        }
-        else{ //Se muestran participantes
+    }else{ //Se muestran participantes
             participantes();
         }
-    }
     return EXIT_SUCCESS;
 }
 
