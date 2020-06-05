78c49a3b0c83da8e6ffffa246bdf9122ecc9a0ab
Jennifer Portiño <jennifer.portinof@utem.cl>
2020-06-04 21:05:35 -0400

    regreso

--- Carrera.h
+++ Carrera.h
@@ -4,23 +4,24 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "Funciones.h"
 
 class Carrera{
     private:
         int vacantes;
-        std::vector<std::string> postulantes;
+        std::vector<Postulante> P; 
         float ultimo;
         std::vector<int> ponderacion;
     public:
         Carrera();
         //Funciones para obtener
         int GetVacantes();
-        std::vector<std::string> GetPostulantes();
+        Postulante GetPostulantes(int);
         float GetUltimo();
         std::vector<int> GetPonderacion();
         //Funciones para agregar
         void SetVacantes(int);
-        void SetPostulantes(std::string,int);
+        void SetPostulantes(int,float, int);
         void SetUltimo(float);
         void SetPonderacion(int,int,int,int,int);
 };
--- main.cpp
+++ main.cpp
@@ -8,14 +8,66 @@
 #include "Funciones.h"
 
 using namespace std;
-
+/**
+ * Escritura del archivo
+ * codigocarrera.txt almacenado en la ubicacion especificada
+*/
 int main(int argc, char** argv){
+    std::vector<Postulante> P;
     if(argc > 1){
         std::string op = argv[1];
         if(op == "1" && argc == 4){ //argc = 4 o 3 (llamada;opcion;ubicacion;ubicacion)
+            LlenarDatos(); //RETURN VECTOR CA[];
             std::string archivo(argv[2]);
-            std::ifstream lectura(archivo);
-            std::ofstream escritura("puntajes.csv");
+            std::ifstream lectura(archivo); //Lectura del archivo puntajes.cvs
+            //Escritura de los .txt por carrera 
+            std::ofstream Biotec("21073.txt");//Ing. Biotecnologia
+            std::ofstream Compu("21041.txt");//Ing. civil computacion
+            std::ofstream CsDatos("21049.txt");//Ing. civil ciencias de datos
+            std::ofstream Arqui("21047.txt");//Arquitectura
+            std::ofstream AdmiP("21089.txt");//Administracion Publica
+            std::ofstream Social("21043.txt");//Trabajo Social
+            std::ofstream Info("21030.txt");//Ing. informatica
+            std::ofstream Meca("21096.txt");//Ing. civil Mecanica
+            std::ofstream CsIng("21046.txt");//Bachillerato Ciencias ingenieria
+            std::ofstream CIndus("21076.txt");//Ing. civil industrial
+            std::ofstream Elec("21075.txt");//Ing. civil electronica
+            std::ofstream Cmrcial("21048.txt");//Ing. comercial
+            std::ofstream Proyec("21071.txt");//Dibujante proyectista
+            std::ofstream Geo("21031.txt");//Ing. geomensura
+            std::ofstream Contru("21032.txt");//Ing. construccion 
+            std::ofstream Indus("21045.txt");//Ing. Industrial
+            std::ofstream Civil("21074.txt");//Ing. civil obras civiles
+            std::ofstream Quim("21083.txt");//Quimica industrial
+            std::ofstream Alim("21039.txt");//Ing. industria alimentaria 
+            std::ofstream Preven("21087.txt");//Ing. civil prevencion de riesgos y medioambiente
+            std::ofstream Agro("21015.txt");//Ing. administracion agroindustrial
+            std::ofstream Inter("21081.txt");//Ing. comercio internacional
+            std::ofstream Biblio("21002.txt");//Bibliotecnologia y documentacion
+            std::ofstream Conta("21012.txt");//Contador publico y auditor
+            std::ofstream IQuim("21080.txt");//Ing. Quimica
+            std::ofstream Turis("21082.txt");//Ing. gestion turistica
+            std::ofstream Visual("21024.txt");//Diseño comunicacion visual
+            std::ofstream Dindus("21023.txt");//diseño industrial
+
+            if (lectura) {
+                for (std::string linea; getline(lectura,linea) ; ){
+                    std::vector<int> persona = obtenerlinea(linea);
+                    
+                    //llenarPostulante(persona, P);
+                    //Aqui quede...
+
+                    //Final
+                    persona.clear();
+                    P.erase (P.begin());
+                }
+                /*for(int i=0; i<cont;i++){ //Creamos el archivo ordenado
+                    B=E[i];
+
+                    std::string salida = std::to_string(B.GetRut()) + ";" + std::to_string(B.GetPromedio());
+                    escritura << salida << std::endl;
+                }*/
+            }
             //ACA EJECUTAR OPCION 1
             std::cout << std::endl << "CORRE 1" << std::endl;
         }else{
