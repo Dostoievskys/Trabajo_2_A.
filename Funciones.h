a4c47c4c167973a08f455cadac3c207a5e349a3b
Jennifer Portiño <jennifer.portinof@utem.cl>
2020-06-13 16:41:31 -0400

    Add: Busqueda

--- Funciones.h
+++ Funciones.h
@@ -9,5 +9,6 @@ std::vector<Carrera> LlenarDatos();
 std::vector<int> obtenerlinea(std::string fila);
 float Ponderacion(Carrera C, std::vector<int> puntajes);
 bool ValidarRut(std::string rut);
+bool Busqueda(std::string r, std::string arch);
 
 #endif
--- main.cpp
+++ main.cpp
@@ -54,12 +54,32 @@ int main(int argc, char** argv){
             if(op == "2" && argc == 4){
                 std::string rut = argv[2];
                 if(ValidarRut(rut)==true){
-                  //ACA EJECUTAR OPCION 2: BUSQUEDA POR RUT
-                }else{
+                    std::string text[28] = {"21073.txt","21041.txt","21049.txt","21047.txt","21089.txt","21043.txt","21030.txt","21096.txt",
+                    "21046.txt","21076.txt","21075.txt","21048.txt","21071.txt","21031.txt","21032.txt","21045.txt","21074.txt","21083.txt",
+                    "21039.txt","21087.txt","21015.txt","21081.txt","21002.txt","21012.txt","21080.txt","21082.txt","21024.txt","21023.txt"};
+                    /**
+                    * Busqueda de un rut
+                    * Pregunta: [programa 2 14123123 ruta_de_archivos]
+                    * [1]: Opcion 2
+                    * [2]: Rut a buscar
+                    * [3]: Ruta donde buscar
+                    */
+                    std::string carpeta(argv[3]); //La arpeta donde estan los archivos
+                    bool Resultado;
+                    for (int i=0; i<28;i++){
+                        std::string rute(carpeta + "/" + text[i]);
+                        Resultado = Busqueda(rut,rute);
+                    }
+                    if(Resultado == false){
+                        cout<<"No se encontro el rut"<<std::endl;
+                    }
+                }
+                else{
                   std::cout << std::endl << "RUT INVALIDO" << std::endl;
                 }
                 std::cout << std::endl << "CORRE 2" << std::endl;
-            }else{
+            }
+            else{ //Validacion de la entrada
                 if(argc != 4){
                     if(op == "2" || op == "1"){
                         std::cout << std::endl << "Ingrese la cantidad correcta de datos" << std::endl;
