1f77c7497f5256b7152bf9bc80d104ce8bf2cb51
Jennifer Portiño <jennifer.portinof@utem.cl>
2020-06-18 00:18:04 -0400

    Add: Creacion de archivos

--- main.cpp
+++ main.cpp
@@ -7,25 +7,25 @@
 #include "Carrera.h"
 #include "Funciones.h"
 
-//Probar con ./dist/programa 1 ./archivos/puntajes.txt ./archivos/
+//Probar con ./dist/programa 1 ./archivos/puntajes.txt ./carreras/
 
 int main(int argc, char** argv){
-    std::vector<Postulante> P;
+    //Arreglo con los codigos de las carreras
+    std::string codigo[28] = {"21089.txt","21002.txt","21012.txt","21048.txt","21015.txt","21081.txt","21082.txt","21047.txt",
+    "21032.txt","21074.txt","21087.txt","21073.txt","21039.txt","21083.txt","21080.txt","21024.txt","21023.txt","21043.txt",
+    "21049.txt","21041.txt","21030.txt","21096.txt","21046.txt","21076.txt","21075.txt","21071.txt","21031.txt","21045.txt"};
+    std::vector<Postulante> P; //Vector para postulantes
+    std::string op = argv[1]; //Opcion que ingresa el usuario
+    Carrera Ca[28]; //Arreglo con los datos de las carreras
     if(argc > 1){
-        std::string op = argv[1];
-        Carrera Ca[28];
         if(op == "1" && argc == 4){
-            std::cout << std::endl << "CORRE 1" << std::endl;
-            LlenarDatos(Ca);
-            std::string arch(argv[2]);
-            std::ifstream lectura(arch);
-            std::ofstream uno("uno.txt"); 
-            std::ofstream dos("dos.txt");
-            std::ofstream tres("tres.txt");
-            if (lectura) { 
+            LlenarDatos(Ca);//Se llenan los datos de las carreras
+            std::string arch(argv[2]); //el archivo que hay que leer
+            std::ifstream lectura(arch); //Lectura
+            std::string carpeta(argv[3]); //Carpeta donde hay que guardar los archivos que se crean
+            if (lectura){ 
                 std::vector<int> persona;
-                  
-                for (std::string linea; getline(lectura,linea) ; ) {
+                for (std::string linea; getline(lectura,linea) ; ){ //Se obtiene la linea, se llena postulante y se añade al vector p
                     persona = obtenerlinea(linea);
                     Postulante A=llenarPostulante(persona);
                     P.push_back(A);
@@ -33,17 +33,13 @@ int main(int argc, char** argv){
                 }
                 entraste(Ca,P);
 
-                for(int j=0; j < Ca[22].GetActVacantes();j++){ //Creamos el archivo ordenado
-                    std::string salida = std::to_string(Ca[22].GetPostulantes(j).rut) + ";" + std::to_string(Ca[22].GetPostulantes(j).pond);
-                    uno << salida << std::endl;
-                }
-                for(int q=0; q < Ca[15].GetActVacantes();q++){ //Creamos el archivo ordenado
-                    std::string salida = std::to_string(Ca[15].GetPostulantes(q).rut) + ";" + std::to_string(Ca[15].GetPostulantes(q).pond);
-                    dos << salida << std::endl;
-                }
-                for(int j=0; j < Ca[16].GetActVacantes();j++){ //Creamos el archivo ordenado
-                    std::string salida = std::to_string(Ca[16].GetPostulantes(j).rut) + ";" + std::to_string(Ca[16].GetPostulantes(j).pond);
-                    tres << salida << std::endl;
+                for (int i=0; i<28;i++){
+                    std::string rute(carpeta + "/" + codigo[i]);//Nombre archivo
+                    std::ofstream crear(rute); //Creacion del archivo
+                    for(int j=0; j < Ca[i].GetActVacantes();j++){ //Creamos el archivo ordenado
+                        std::string salida = std::to_string(Ca[i].GetPostulantes(j).rut) + ";" + std::to_string(Ca[i].GetPostulantes(j).pond);
+                        crear << salida << std::endl;
+                    }
                 }
             }
 
